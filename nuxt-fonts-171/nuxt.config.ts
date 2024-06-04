3d9477cb4c2daefcd0c753fb5438933f18342cb8
☁️dungsil <iam@younggeon.kim>
2024-06-04 20:34:57 +0900

    nuxt/fonts#171

---
+++ nuxt-fonts-171/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  srcDir: 'src',
+
+  css: ['assets/css/global.css'],
+
+  devtools: { enabled: true },
+  modules: ['@nuxt/fonts'],
+})
