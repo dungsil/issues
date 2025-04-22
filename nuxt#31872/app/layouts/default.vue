c2c1371cf2f131c8f0d0fea319988c4ec2ccfc5e
☁️dungsil <iam@younggeon.kim>
2025-04-22 14:41:38 +0900

    Add files via upload

---
+++ nuxt#31872/app/layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Default layout</h1>
+    <slot></slot>
+  </div>
+</template>
---
+++ nuxt#31872/nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+
+  future: {
+    compatibilityVersion: 4,
+    typescriptBundlerResolution: true,
+  },
+
+  experimental: {
+    buildCache: true,
+  },
+})
